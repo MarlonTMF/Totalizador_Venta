c8e7b84b9a692bccdc9957c20e8d0ea672a18e7f
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2025-11-06 12:19:31 -0400

    1ra Orueba Totalizador2

--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -4,4 +4,7 @@ describe("Items_Total", () => {
   it("debería calcular el total para 6 items a precio 5", () => {
     expect(Items_Total(6, 5)).toBe(30);
   });
+  it("debería calcular el total para 3 items a precio 10", () => {
+  expect(Items_Total(3, 10)).toBe(30);
+});
 });
